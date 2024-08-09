9f1831812220bc8a2c407dd3b0201ee2b6614057
Onitsiky <hei.onitsiky@gmail.com>
2024-08-09 15:29:07 +0300

    feat: sam app with docker

---
+++ jcloudify/app.py
@@ -0,0 +1,15 @@
+import json
+import subprocess
+
+
+def lambda_handler(event, context):
+    result = subprocess.run(['sam', '--version'], capture_output=True, text=True)
+    print(result.stdout)
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            {
+                "message": "hello world",
+            }
+        ),
+    }
