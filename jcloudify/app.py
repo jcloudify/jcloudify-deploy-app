df07e3cd89058390cf96b2249e155509d15c6e37
Onitsiky <hei.onitsiky@gmail.com>
2024-08-09 15:47:20 +0300

    chore: test sam cli integration

--- jcloudify/app.py
+++ jcloudify/app.py
@@ -3,7 +3,7 @@ import subprocess
 
 
 def lambda_handler(event, context):
-    result = subprocess.run(['sam', '--version'], capture_output=True, text=True)
+    result = subprocess.run(["sam", "--version"], capture_output=True, text=True)
     print(result.stdout)
     return {
         "statusCode": 200,
