cac8164d4721090f94991640f79b94142a9622b6
Onitsiky <hei.onitsiky@gmail.com>
2024-08-26 14:04:00 +0300

    chore: refactor code

--- jcloudify/app.py
+++ jcloudify/app.py
@@ -17,14 +17,7 @@ def lambda_handler(event, context):
         print(f"Received records: {json.dumps(records)}")
         body = json.loads(records["body"])
         detail = body["detail"]
-        app_name = detail.get("app_name")
-        env_name: str = detail.get("environment_type")
-        bucket_key = detail.get("formatted_bucket_key")
-        app_id = detail.get("app_id")
-        user_id = detail.get("user_id")
-        env_id = detail.get("env_id")
-        process(app_name, env_name.lower(), bucket_key)
-        send_event(user_id, app_id, env_id, env_name.lower(), app_name)
+        process_deployment(detail)
 
     return {
         "statusCode": 200,
@@ -51,18 +44,21 @@ def check_if_file_exists(bucket_name, key):
             raise e
 
 
-def download_file_from_bucket(bucket_name, key):
-    file_exists = check_if_file_exists(bucket_name, key)
+def download_file_from_bucket(bucket_key):
+    bucket_name = os.getenv("AWS_S3_BUCKET_NAME")
+    file_exists = check_if_file_exists(bucket_name, bucket_key)
     try:
-        destination_file_path = f"/tmp/{get_filename_from_bucket_key(key)}"
+        destination_file_path = f"/tmp/{get_filename_from_bucket_key(bucket_key)}"
         if file_exists:
-            print(f"Downloading {key} to {destination_file_path}")
-            get_s3_client().download_file(bucket_name, key, destination_file_path)
+            print(f"Downloading {bucket_key} to {destination_file_path}")
+            get_s3_client().download_file(
+                bucket_name, bucket_key, destination_file_path
+            )
             return destination_file_path
         else:
-            raise FileExistsError(f"The file {key} does not exist.")
+            raise FileExistsError(f"The file {bucket_key} does not exist.")
     except ClientError as e:
-        print(f"Error downloading file {key}: {e}")
+        print(f"Error downloading file {bucket_key}: {e}")
         raise e
 
 
@@ -141,7 +137,13 @@ def send_event(user_id, app_id, env_id, env, app_name):
     print(response)
 
 
-def deploy_app(app_name, env):
+def get_built_project_from_s3(bucket_key):
+    zip_build_file = download_file_from_bucket(bucket_key)
+    unzip_build_file(zip_build_file, TMP_DIR_PATH)
+    set_write_permission(f"{TMP_DIR_PATH}/.aws-sam")
+
+
+def trigger_app_deployment(app_name, env):
     stack_name = f"{env}-compute-{app_name}"
     print(f"Deploying {stack_name}")
     deployment_command = [
@@ -153,9 +155,17 @@ def deploy_app(app_name, env):
     print(execute_commands(deployment_command))
 
 
-def process(app_name, env, bucket_key):
-    bucket_name = os.getenv("AWS_S3_BUCKET_NAME")
-    zip_build_file = download_file_from_bucket(bucket_name, bucket_key)
-    unzip_build_file(zip_build_file, TMP_DIR_PATH)
-    set_write_permission(f"{TMP_DIR_PATH}/.aws-sam")
-    deploy_app(app_name, env)
+def deploy_app(app_name, env, bucket_key):
+    get_built_project_from_s3(bucket_key)
+    trigger_app_deployment(app_name, env)
+
+
+def process_deployment(event_details):
+    app_name = event_details.get("app_name")
+    env_name: str = event_details.get("environment_type")
+    bucket_key = event_details.get("formatted_bucket_key")
+    app_id = event_details.get("app_id")
+    user_id = event_details.get("user_id")
+    env_id = event_details.get("env_id")
+    deploy_app(app_name, env_name.lower(), bucket_key)
+    send_event(user_id, app_id, env_id, env_name.lower(), app_name)
